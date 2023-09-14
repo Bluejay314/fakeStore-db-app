8ee8e9333ce9932a97c1f8372bbab0211d8419a6
Jarrad <bluejay.jzp@gmail.com>
2023-09-15 07:50:03 +1000

    added jsdoc

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,25 +1,51 @@
+/**
+ * @module controllers/productController
+ * @description This model handles the manipulation of product data based on route take by the user
+ */
+
 "use strict";
 
+const express = require("express");
 let { Product } = require("../models");
 
+/**
+ * @description Retrieves a list of all products in the database
+ * @param {express.Response} res server response object
+ */
 const getProducts = (res) => {
     Product.find({})
         .then((data) => res.send({ result: 200, data: data }))
         .catch((err) => res.send({ result: 500, error: err.message }));
 };
 
+/**
+ * @description Retrieves a single product with the id specified by the URL params.
+ * @param {express.Request} req server request object
+ * @param {express.Response} res server response object
+ */
 const getProduct = (req, res) => {
     Product.findById(req.params.id)
         .then((data) => res.send({ result: 200, data: data }))
         .catch((err) => res.send({ result: 500, error: err.message }));
 };
 
+/**
+ * @description Creates a new product filled wit the data provided in the request body.
+ * @param {Object} data body of the express request object
+ * @param {express.Response} res server response object
+ */
 const createProduct = (data, res) => {
     new Product(data).save()
         .then((data) => res.send({ result: 200, data: data }))
         .catch((err) => res.send({ result: 500, error: err.message }));
 };
 
+/**
+ * @description Finds a product with the id specified in the url params. Updates with
+ *              the data provided by the request body.
+ * @param {express.Request} req server request object
+ * @param {express.Response} res server response object
+ */
 const updateProduct = (req, res) => {
     Product.findByIdAndUpdate(req.params.id, req.body, {
         useFindAndModify: false,
@@ -28,6 +54,11 @@ const updateProduct = (req, res) => {
         .catch((err) => res.send({ result: 500, error: err.message }));
 };
 
+/**
+ * @description Deletes a single product that has the id provided by request params.
+ * @param {express.Request} req server request object
+ * @param {express.Response} res server response object
+ */
 const deleteProduct = (req, res) => {
     Product.findByIdAndRemove(req.params.id, req.body, {
         useFindAndModify: false,
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,41 @@
+/**
+ * @module controllers/userController
+ * @description This model handles the manipulation of user data based on route take by the user
+ */
+
 "use strict";
 
 const express = require("express");
 const bcrypt = require("bcrypt");
-
 let { User } = require("../models");
 
+/**
+ * @description Retrieves a single user that has the id provided by request params.
+ * @param {express.Request} req server request object
+ * @param {express.Response} res server response object
+ */
 const getUser = (req, res) => {
     User.findById(req.params.id)
         .then((data) => res.send({ result: 200, data: data }))
         .catch((err) => res.send({ result: 500, error: err.message }));
 }
 
+/**
+ * @description Retrieves a list of all the users in the database.
+ * 
+ * @param {express.Response} res 
+ */
 const getUsers = (res) => {
     User.find({})
         .then((data) => res.send({ result: 200, data: data }))
         .catch((err) => res.send({ result: 500, error: err.message }));
 };
 
+/**
+ * @description Creates a new user filled wit the data provided in the request body.
+ * @param {Object} data body of the express request object
+ * @param {express.Response} res server response object
+ */
 const createUser = (data, res) => {
     bcrypt.hash(data.password, 10).then(hash => {
         new User({...data, password: hash}).save()
@@ -25,6 +44,12 @@ const createUser = (data, res) => {
     });
 };
 
+/**
+ * @description Finds a user with the id specified in the url params. Updates with
+ *              the data provided by the request body.
+ * @param {express.Request} req server request object
+ * @param {express.Response} res server response object
+ */
 const updateUser = (req, res) => {
     User.findByIdAndUpdate(req.params.id, req.body, {
         useFindAndModify: false,
@@ -33,6 +58,11 @@ const updateUser = (req, res) => {
         .catch((err) => res.send({ result: 500, error: err.message }));
 };
 
+/**
+ * @description Deletes a single user that has the id provided by request params.
+ * @param {express.Request} req server request object
+ * @param {express.Response} res server response object
+ */
 const deleteUser = (req, res) => {
     User.findByIdAndRemove(req.params.id, req.body, {
         useFindAndModify: false,
